2b6d9bc2ad93da495c58e3d76d920204ce325704
=nefremova <nefremova0@gmail.com>
2018-04-26 22:21:33 -0700

    Add missing chenges from the main class.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		ArrayList<Student> students; // = Read students from student file
+		// Create student/faculty ID generator class
+		
+		ArrayList<Student> students; // = Read students from student file (using ID generator)
 		ArrayList<Faculty> faculty;  // = Read faculty from faculty file 
 		ArrayList<Course> courses;   // = Read courses from courses file 
 		ScheduleParameters parameters; // = Read parameters from paramenter file 
